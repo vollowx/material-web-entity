686d4920895fce15330e3a2d8cac74676be3dba4
huge-pancake <huge.pancake.zxr@gmail.com>
2022-05-20 20:41:09 +0800

    no focus-visible.js & zip less size

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ declare global {
 
 import * as mComp from './components/index';
 import * as mUtil from './utils/index';
-import './libs/focus-visible.js';
 
 export { mComp, mUtil };
 onload = () => {
@@ -16,16 +15,16 @@ onload = () => {
   if (window.mComp && window.mUtil) {
     console.log(
       '%cMWE%cConstant defining%csuccessful',
-      'padding: 4px; color: #8c7af1; font-weight: 600; background: #171717; border-radius: 4px 0 0 4px;',
-      'padding: 4px; color: #aaa; background: #212121;',
-      'color: #53e199; padding: 4px; background: #333333; border-radius: 0 4px 4px 0;'
+      'padding:4px;color:#8c7af1;font-weight:600;background:#171717;border-radius:4px 0 0 4px;',
+      'padding:4px;color:#aaa;background:#212121;',
+      'color:#53e199;padding:4px;background:#333333;border-radius:0 4px 4px 0;'
     );
   } else {
     console.log(
       '%cMWE%Constant defining%cfailed',
-      'padding: 4px; color: #8c7af1; font-weight: 600; background: #171717; border-radius: 4px 0 0 4px;',
-      'padding: 4px; color: #aaa; background: #212121;',
-      'color: #ff0000; padding: 4px; background: #333333; border-radius: 0 4px 4px 0;'
+      'padding:4px;color:#8c7af1;font-weight:600;background:#171717;border-radius:4px 0 0 4px;',
+      'padding:4px;color:#aaa;background:#212121;',
+      'color:#ff0000;padding:4px;background:#333333;border-radius:0 4px 4px 0;'
     );
   }
 };
