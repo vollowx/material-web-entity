d286c7c850d3633e96155d235f26818aa9c3a664
huge-pancake <huge.pancake.zxr@gmail.com>
2022-06-10 17:49:52 +0800

    chore(Menu): separation logic of opening

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -33,44 +33,7 @@ class M3Menu extends HTMLElement {
         ))
       : null;
 
-    this.addEventListener('keydown', (e) => {
-      if (e.key == 'ArrowDown' || e.key == 'ArrowUp') {
-        // Focus moving
-        e.preventDefault();
-        let focusItem = this.querySelector('md-menu-item:focus') as HTMLButtonElement;
-        let items = this.querySelectorAll('md-menu-item') as NodeListOf<HTMLButtonElement>;
-        let index = [].indexOf.call(items, focusItem);
-        e.key == 'ArrowDown' ? index++ : index--;
-        if (index < 0) {
-          index = items.length - 1;
-        } else if (index >= items.length) {
-          index = 0;
-        }
-        items[index].tabIndex = 0;
-        items[index].focus();
-        focusItem.tabIndex = -1;
-      } else if (e.key == 'Escape') {
-        // Menu closing
-        e.preventDefault();
-        this.closeMenu();
-      } else if (e.key == 'ArrowLeft') {
-        // Submenu closing
-        if (this.sub) {
-          e.preventDefault();
-          this.closeMenu();
-        }
-      } else if (e.key == 'Enter' || e.key == 'ArrowRight' || e.key == ' ') {
-        // Submenu opening
-        let focusItem = this.querySelector('md-menu-item:focus');
-        if (focusItem.hasAttribute('subber')) {
-          e.preventDefault();
-          (document.querySelector(`md-menu#${focusItem.id}`) as M3Menu).openMenu();
-        }
-      } else if (e.key == 'Tab') {
-        // Blur as Menu closing
-        this.closeMenu();
-      }
-    });
+    this.addEventListener('keydown', this.onKeyAction);
     document.addEventListener('click', (e: Event) => {
       if (this.open && !this.contains(e.target as HTMLElement) && e.target !== this.controllerElement) {
         this.closeMenu();
@@ -158,6 +121,109 @@ class M3Menu extends HTMLElement {
   }
 
   /**
+   * The actions when the user press a key.
+   */
+  onKeyAction(e: KeyboardEvent) {
+    if (e.key == 'ArrowDown' || e.key == 'ArrowUp') {
+      // Focus moving
+      e.preventDefault();
+      let focusItem = this.querySelector('md-menu-item:focus') as HTMLButtonElement;
+      let items = this.querySelectorAll('md-menu-item') as NodeListOf<HTMLButtonElement>;
+      let index = [].indexOf.call(items, focusItem);
+      e.key == 'ArrowDown' ? index++ : index--;
+      if (index < 0) {
+        index = items.length - 1;
+      } else if (index >= items.length) {
+        index = 0;
+      }
+      items[index].tabIndex = 0;
+      items[index].focus();
+      focusItem.tabIndex = -1;
+    } else if (e.key == 'Escape') {
+      // Menu closing
+      e.preventDefault();
+      this.closeMenu();
+    } else if (e.key == 'ArrowLeft') {
+      // Submenu closing
+      if (this.sub) {
+        e.preventDefault();
+        this.closeMenu();
+      }
+    } else if (e.key == 'Enter' || e.key == 'ArrowRight' || e.key == ' ') {
+      // Submenu opening
+      let focusItem = this.querySelector('md-menu-item:focus');
+      if (focusItem.hasAttribute('subber')) {
+        e.preventDefault();
+        (document.querySelector(`md-menu#${focusItem.id}`) as M3Menu).openMenu();
+      }
+    } else if (e.key == 'Tab') {
+      // Blur as Menu closing
+      this.closeMenu();
+    }
+  }
+  /**
+   * Calculate the position of the menu.
+   */
+  protected calculatePosition(): {
+    top: number;
+    left: number;
+    right: number;
+    bottom: number;
+    horizontal: boolean;
+    vertical: boolean;
+  } {
+    let _position: { top: number; left: number; right: number; bottom: number } = {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    };
+    let _classes: { horizontal: boolean; vertical: boolean } = {
+      horizontal: false,
+      vertical: false,
+    };
+
+    const rect = this.controllerElement.getBoundingClientRect();
+
+    if (rect.top + rect.height / 2 > window.innerHeight / 2) {
+      _classes.vertical = true;
+      if (this.sub) {
+        _position.bottom = window.innerHeight - rect.top - rect.height - 8;
+      } else {
+        _position.bottom = window.innerHeight - rect.top;
+      }
+      if (this.menuElement.offsetTop < rect.height) {
+        _position.top = rect.height;
+      }
+    } else {
+      if (this.sub) {
+        _position.top = rect.top - 8;
+      } else {
+        _position.top = rect.top + rect.height;
+      }
+      if (window.innerHeight - this.menuElement.offsetTop - this.menuElement.offsetHeight < rect.height) {
+        _position.bottom = rect.height;
+      }
+    }
+    if (this.sub) {
+      if (rect.left + rect.width + this.menuElement.offsetWidth > window.innerWidth) {
+        _position.left = rect.right - 48 - this.menuElement.offsetWidth;
+        _classes.horizontal = true;
+      } else {
+        _position.left = rect.left + rect.width;
+      }
+    } else {
+      if (rect.left + rect.width + this.menuElement.offsetWidth > window.innerWidth) {
+        _position.left = rect.right - this.menuElement.offsetWidth;
+        _classes.horizontal = true;
+      } else {
+        _position.left = rect.left;
+      }
+    }
+
+    return { ..._position, ..._classes };
+  }
+  /**
    * Set the menu position.
    * ! Need more testing.
    * TODO: Need more position.
@@ -171,42 +237,22 @@ class M3Menu extends HTMLElement {
     );
     this.menuElement.removeAttribute('style');
     this.menuElement.classList.remove('md-menu--bottom', 'md-menu--right');
-    let rect = this.controllerElement.getBoundingClientRect();
-    if (rect.top + rect.height / 2 > window.innerHeight / 2) {
+
+    const { top, left, right, bottom, horizontal, vertical } = this.calculatePosition();
+
+    if (vertical) {
       this.menuElement.classList.add('md-menu--bottom');
-      if (this.sub) {
-        this.menuElement.style.bottom = window.innerHeight - rect.top - rect.height - 8 + 'px';
-      } else {
-        this.menuElement.style.bottom = window.innerHeight - rect.top + 'px';
-      }
-      if (this.menuElement.offsetTop < rect.height) {
-        this.menuElement.style.top = rect.height + 'px';
-      }
+      this.menuElement.style.bottom = `${bottom}px`;
     } else {
-      if (this.sub) {
-        this.menuElement.style.top = rect.top - 8 + 'px';
-      } else {
-        this.menuElement.style.top = rect.top + rect.height + 'px';
-      }
-      if (window.innerHeight - this.menuElement.offsetTop - this.menuElement.offsetHeight < rect.height) {
-        this.menuElement.style.bottom = rect.height + 'px';
-      }
+      this.menuElement.style.top = `${top}px`;
     }
-    if (this.sub) {
-      if (rect.left + rect.width + this.menuElement.offsetWidth > window.innerWidth) {
-        this.menuElement.style.left = rect.right - 48 - this.menuElement.offsetWidth + 'px';
-        this.menuElement.classList.add('md-menu--right');
-      } else {
-        this.menuElement.style.left = rect.left + rect.width + 'px';
-      }
+    if (horizontal) {
+      this.menuElement.classList.add('md-menu--right');
+      this.menuElement.style.right = `${right}px`;
     } else {
-      if (rect.left + rect.width + this.menuElement.offsetWidth > window.innerWidth) {
-        this.menuElement.style.left = rect.right - this.menuElement.offsetWidth + 'px';
-        this.menuElement.classList.add('md-menu--right');
-      } else {
-        this.menuElement.style.left = rect.left + 'px';
-      }
+      this.menuElement.style.left = `${left}px`;
     }
+
     this.open = true;
     (this.querySelector('md-menu-item') as HTMLButtonElement).tabIndex = 0;
     (this.querySelector('md-menu-item') as HTMLButtonElement).focus();
